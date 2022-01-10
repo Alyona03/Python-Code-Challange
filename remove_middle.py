6ea1832acc4f91936b733718df199b22e5507c77
Alona Iavorska <alyona3jaworska@gmail.com>
2022-01-10 14:43:33 -0500

    Create remove_middle.py

---
+++ remove_middle.py
@@ -0,0 +1,10 @@
+#Create a function named remove_middle which has three parameters named lst, start, and end.
+#The function should return a list where all elements in lst with an index between start and end (inclusive) have been removed.
+#For example, the following code should return [4, 23, 42] because elements at indices 1, 2, and 3 have been removed:
+
+
+
+def remove_middle(lst, start, end):
+   return lst[:start] + lst[end+1:]
+
+print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
